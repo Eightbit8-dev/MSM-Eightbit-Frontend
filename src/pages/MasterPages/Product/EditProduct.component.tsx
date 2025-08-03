58f5a89de3bbae2c7a80954edb0103eb3862e995
sachin9345 <freeforerfie@gmail.com>
2025-08-03 20:43:21 +0530

    import functions

--- src/pages/MasterPages/Product/EditProduct.component.tsx
+++ src/pages/MasterPages/Product/EditProduct.component.tsx
@@ -8,6 +8,7 @@ import {
   useCreateProduct,
   useEditProduct,
 } from "../../../queries/masterQueries/ProductQuery";
+import ImportModal from "./ProductImportModal";
 
 const ProductEdit = ({
   productDetails,
@@ -21,6 +22,7 @@ const ProductEdit = ({
   setProduct: React.Dispatch<React.SetStateAction<ProductDetails>>;
 }) => {
   const [productData, setProductData] = useState<ProductDetails | null>(null);
+  const [isImportModalOpen, setIsImportModalOpen] = useState(false);
 
   const { mutate: createProduct, isPending, isSuccess } = useCreateProduct();
 
@@ -84,6 +86,10 @@ const ProductEdit = ({
     setProductData(resetData);
   };
 
+  const handleImportClick = () => {
+    setIsImportModalOpen(true);
+  };
+
   if (!productData) {
     return (
       <p className="text-center text-sm text-gray-500">
@@ -140,14 +146,25 @@ const ProductEdit = ({
               )}
 
               {formState === "create" && (
-                <ButtonSm
-                  className="font-medium text-white"
-                  text={isPending ? "Creating..." : "Create New"}
-                  state="default"
-                  isPending={isPending}
-                  type="submit"
-                  disabled={isPending}
-                />
+                <>
+                  <ButtonSm
+                    className="font-medium text-white opacity-100"
+                    text="Import"
+                    state="default"
+                    type="button"
+                    iconPosition="right"
+                    imgUrl="/icons/ArrowDown.svg"
+                    onClick={handleImportClick}
+                  />
+                  <ButtonSm
+                    className="font-medium text-white"
+                    text={isPending ? "Creating..." : "Create New"}
+                    state="default"
+                    isPending={isPending}
+                    type="submit"
+                    disabled={isPending}
+                  />
+                </>
               )}
 
               {formState === "edit" && (
@@ -235,6 +252,11 @@ const ProductEdit = ({
           </div>
         </form>
       </div>
+
+      <ImportModal
+        isOpen={isImportModalOpen}
+        onClose={() => setIsImportModalOpen(false)}
+      />
     </main>
   );
 };
