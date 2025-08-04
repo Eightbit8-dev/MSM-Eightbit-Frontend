a26fc449fdf9bffd52f37afe8cafb4a055c40fde
sabarish2317 <Sabarish_7@protonmail.com>
2025-08-04 14:21:24 +0530

    added change sin home page

--- src/pages/Transaction/serviceEntry/ServiceEntry.tsx
+++ src/pages/Transaction/serviceEntry/ServiceEntry.tsx
@@ -39,22 +39,24 @@ const ServiceEntryPage = () => {
         {isLoading ? (
           <EmployeeTableSkeleton />
         ) : (
-          <div className="flex flex-col items-start justify-start gap-2 overflow-clip rounded-[12px] bg-white/80 md:p-4">
-            <div className="flex w-full items-center justify-between p-2 md:p-4">
+          <div className="flex flex-col items-start justify-start gap-2 overflow-clip rounded-[12px] bg-white/80 py-3 md:p-4">
+            <div className="flex w-full items-center justify-between px-3 md:px-0">
               <section className="result-length flex w-full flex-row items-center justify-between gap-2">
                 <div className="flex items-center gap-2">
-                  <div className="flex h-[10px] w-[10px] rounded-full bg-blue-500"></div>
-                  <h2 className="text-md font-semibold text-zinc-800">
+                  <div className="h-[10px] w-[10px] rounded-full bg-blue-500"></div>
+                  <h2 className="text-md min-w-max font-semibold text-zinc-800">
                     Showing {paginatedData.length} results of{" "}
                     {data?.totalRecords || 0}
                   </h2>
                 </div>
 
-                <PaginationControls
-                  totalPages={totalPages}
-                  currentPage={currentPage}
-                  onPageChange={setCurrentPage}
-                />
+                <div className="flex items-center gap-2">
+                  <PaginationControls
+                    totalPages={totalPages}
+                    currentPage={currentPage}
+                    onPageChange={setCurrentPage}
+                  />
+                </div>
               </section>
             </div>
 
